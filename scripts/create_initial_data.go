95c08a133e705207ea3578d401dd9ec96aeee2b7
hoshinaoshi <hoshinaoshi@gmail.com>
2018-11-17 12:51:59 +0900

    goroutine使用

--- scripts/create_initial_data.go
+++ scripts/create_initial_data.go
@@ -33,11 +33,8 @@ func main() {
 
   reader := csv.NewReader(fp)
   reader.Comma = '\t'
-
-  // コメント設定(なんとコメント文字を指定できる!)
   reader.Comment = '#'
 
-  // 全部読みだす
   records, err := reader.ReadAll()
   if err != nil {
     log.Fatal(err)
@@ -47,14 +44,16 @@ func main() {
     Region: aws.String("us-west-2"),
   })
   table := db.Table("dev-synonyms")
-log.Println(table)
-  // 各行でループ
-  for _, row := range records {
-    fmt.Printf("tag: %s, synonyms: %s", row[0], strings.Split(row[5], "/"))
 
+  for _, row := range records {
+    go func() {
+      fmt.Printf("tag: %s, synonyms: %s", row[0], strings.Split(row[5], "/"))
+/*
     synonym := Synonym{Tag: , Synonyms: []string{"b","c"}}
     log.Println(synonym.Synonyms)
     go table.Put(synonym).Run()
+*/
+    }()
   }
-  time.Sleep(3 * time.Second)
+  time.Sleep(10 * time.Second)
 }
